2dcb2c7136d9a79c32c68d547ec8c8806b86c0b1
Admaro <156846255+RoeAdr@users.noreply.github.com>
2024-08-14 15:00:24 +0000

    Add Stuff

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,49 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers  } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
+
+
+// GET (all)
+customersRouter.get("/", async(req, res) => {
+    const customers = await getCustomers();
+    res.json(customers);
+  });
+
+
+// GET /{id}
+customersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const customer = await getCustomerDetail(id);
+    if (customer != null){      
+      res.json(customer);
+    } else {
+      res.status(404).json({message:"Customer Not Found"});
+    }
+  });
+
+// GET /{id}/orders
+customersRouter.get("/:id/orders", async (req, res) => {
+    const id = req.params.id;
+    const orders = await getOrdersForCustomer(id);
+    res.json(orders);
+});
+
+
+// GET /search
+customersRouter.get("/search/:query", async (req, res) => {
+    const query = req.params.query;
+    const customers = await searchCustomers(query);
+    if (customers != null){      
+        res.json(customers);
+      } else {
+        res.status(404).json({message:"No Customers Found"});
+      }
+});
+
+
+
+
+
+
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,73 @@
 import express from "express";
+import { getOrderDetail, getOrders } from "./orders.service";
+import { validate } from "../../middleware/validation.middleware";
+import { idUUIDRequestSchema, pagingRequestSchema } from "../types";
 
 export const ordersRouter = express.Router();
+
+
+// GET (all) with paging (take and skip)
+/*
+    call example: http://localhost:4000/api/orders?take=7&skip=1    
+*/
+
+
+ordersRouter.get("/", validate(pagingRequestSchema), async(req, res) => {
+    const data = pagingRequestSchema.parse(req);
+    const orders = await getOrders(data.query.skip, data.query.take);
+    res.json(orders);
+});
+
+// Old Call Validation can be improved with Middleware
+/*        
+ordersRouter.get("/", async(req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+    if (
+        take
+        && typeof take === "string"
+        && parseInt(take) > 0
+        && skip
+        && typeof skip === "string"
+        && parseInt(skip) > -1
+    ) {
+        const orders = await getOrders(parseInt(skip), parseInt(take));
+        res.json(orders);
+    } else {
+        res.status(400).json({
+            message:
+                "Take and skip query parameters are required. " +
+                "Take must be greater than 0 and skip must be greater than -1",
+        });
+    } 
+  });
+*/
+
+
+// GET /{id}
+
+ordersRouter.get("/:id", validate(idUUIDRequestSchema), async (req, res) => {
+    const data = idUUIDRequestSchema.parse(req);
+    const order = await getOrderDetail(data.params.id);
+    if (order != null) {
+        res.json(order);
+    } else {
+        res.status(404).json({message: "Order Not Found"});
+    }
+});
+
+
+// Old call
+/*
+ordersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const order = await getOrderDetail(id);
+    if (order != null){      
+      res.json(order);
+    } else {
+      res.status(404).json({message:"Order Not Found"});
+    }
+  });
+  */
