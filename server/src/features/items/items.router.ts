017b01459246e621971bdace492f7f55bb09d01a
Admaro <156846255+RoeAdr@users.noreply.github.com>
2024-08-15 14:20:09 +0000

    Add xml to GET

--- server/src/features/items/items.router.ts
+++ server/src/features/items/items.router.ts
@@ -1,16 +1,32 @@
 import express from "express";
 import { deleteItem, getItemDetail, getItems, upsertItem } from "./items.service";
 import { validate } from "../../middleware/validation.middleware";
+import { create } from "xmlbuilder2";
 import { idNumberRequestSchema, itemPOSTRequestSchema, itemPUTRequestSchema } from "../types";
+import { it } from "node:test";
 
 export const itemsRouter = express.Router();
 
+
+// call http://localhost:4000/api/items
+// if xml: set header.Accept to "application/xml"
 itemsRouter.get("/", async(req, res) => {
   const items = await getItems();
   items.forEach((item)=> {
     item.imageUrl = buildImageUrl(req, item.id);
   });
-  res.json(items);
+
+  // Content type xml
+  if (req.headers["accept"] == "application/xml"){
+    const root = create().ele("items");
+    items.forEach((i) => {
+      root.ele("item", i);
+    });
+    res.status(200).send(root.end({prettyPrint: true}));
+    // Note: node needs to use a xml builder library -  C# has one out of the box...
+  } else {
+    res.json(items);
+  }
 });
 
 
@@ -19,9 +35,21 @@ itemsRouter.get("/:id", async (req, res) => {
   const item = await getItemDetail(id);
   if (item != null){
     item.imageUrl = buildImageUrl(req, item.id);
-    res.json(item);
+
+    // Content type xml
+    if (req.headers["accept"] == "application/xml"){
+      res.status(200).send(create().ele("item",item).end());
+    } else {
+      res.json(item);
+    }
   } else {
+    if (req.headers["accept"] == "application/xml"){
+      res
+        .status(400)
+        .send(create().ele("error", {message: "Itemo Not Found"}).end());
+    } else {
     res.status(404).json({message:"Item Not Found"});
+    }
   }
 
 })
