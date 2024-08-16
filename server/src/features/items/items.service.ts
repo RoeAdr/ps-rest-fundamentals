562338614ca411ad1dae676fb30628c50fb32226
Admaro <156846255+RoeAdr@users.noreply.github.com>
2024-08-16 08:52:38 +0000

    Add TTL Caching

--- server/src/features/items/items.service.ts
+++ server/src/features/items/items.service.ts
@@ -1,16 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import { Item, ItemDTO, ItemDetail } from "../types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import TTLCache from "@isaacs/ttlcache";
 
 const prisma = new PrismaClient();
+const options = {ttl: 1000 * 60 * 60 * 24}; // 1 Day in mms
+const cache = new TTLCache(options);
+const listKey = "items-list";
+
 
 export function getItems(): Promise<Item[]> {
+  const cacheItems = cache.get<Item[]>(listKey);
+if (cacheItems != undefined) {
+  return Promise.resolve(cacheItems);
+}
+
   return prisma.item.findMany({
     select: {
       id: true,
       name: true,
     },
-  });
+  })
+  .then((items) => {
+    cache.set(listKey, items);
+    return items;
+  }  
+  );  
 }
 
 export function getItemDetail(itemId: number): Promise<ItemDetail | null> {
